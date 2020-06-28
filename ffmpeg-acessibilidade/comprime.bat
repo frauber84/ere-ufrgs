b96f6be2c680ccf86c760caf9483c610bd93d2e7
Fernando Rauber Gonçalves <36580373+frauber84@users.noreply.github.com>
2020-06-28 18:43:36 -0300

    Add files via upload
    Diversas melhorias

--- ffmpeg-acessibilidade/comprime.bat
+++ ffmpeg-acessibilidade/comprime.bat
@@ -9,9 +9,10 @@ GOTO FINAL
 :PROCESSAR
 ECHO ..........................................................
 ECHO     Comprime v°deos com ffmpeg sem perda de resoluá∆o       
+ECHO         https://github.com/frauber84/ere-ufrgs/
 ECHO ..........................................................
 ECHO[
-ECHO Guia para bitrates (menores valores = mais compress∆o)
+ECHO Guia para bitrates (valores menores = mais compress∆o, menor tamanho do arquivo e qualidade)
 ECHO[
 ECHO Compress∆o extrema = 128 (WhatsApp / 3G)
 ECHO Compress∆o alta = 256 (WhatsApp / 3G)
@@ -21,11 +22,14 @@ ECHO[
 ECHO OBS:
 ECHO 1) N∆o comprima v°deos ao enviar para YouTube, envie na qualidade original.
 ECHO 2) O menor bitrate poss°vel depende da resoluá∆o original. Quanto menor a resoluá∆o,
-ECHO    maior a possibilidade de bitrates menores. Experimente atÈ descobrir o valor adequado
-ECHO    para seus v°deos. Sugiro comeáar com o valor 256.
+ECHO    maior a possibilidade de bitrates menores. Experimente atÇ descobrir o valor adequado
+ECHO    para seus v°deos. Sugere-se comeáar com o valor 256 para avaliar os resultados.
 ECHO[ 
 set /p BITRATE=Digite o bitrate desejado e pressione ENTER:
 ECHO[ 
-ffmpeg -loglevel warning -stats -i %1 -ab 96k -b:v %BITRATE%k -preset veryfast %1_%BITRATE%k.mp4
+ffmpeg -loglevel warning -stats -i %1 -c:v libx264 -ab 80k -b:v %BITRATE%k  -tune stillimage -preset veryfast %1_%BITRATE%k.mp4
+
+REM Acrescentar para converter em 720p quando maior que 720p:
+REM -vf scale='if(gte(ih\,720)\,720\,iw)':-2
 
 :FINAL
--- ffmpeg-acessibilidade/comprime_todos.bat
+++ ffmpeg-acessibilidade/comprime_todos.bat
@@ -1,9 +1,11 @@
 @ECHO OFF
+:PROCESSAR
 ECHO ..........................................................
 ECHO     Comprime v°deos com ffmpeg sem perda de resoluá∆o       
+ECHO         https://github.com/frauber84/ere-ufrgs/
 ECHO ..........................................................
 ECHO[
-ECHO Guia para bitrates (menores valores = mais compress∆o)
+ECHO Guia para bitrates (valores menores = mais compress∆o, menor tamanho do arquivo e qualidade)
 ECHO[
 ECHO Compress∆o extrema = 128 (WhatsApp / 3G)
 ECHO Compress∆o alta = 256 (WhatsApp / 3G)
@@ -13,11 +15,11 @@ ECHO[
 ECHO OBS:
 ECHO 1) N∆o comprima v°deos ao enviar para YouTube, envie na qualidade original.
 ECHO 2) O menor bitrate poss°vel depende da resoluá∆o original. Quanto menor a resoluá∆o,
-ECHO    maior a possibilidade de bitrates menores. Experimente atÈ descobrir o valor adequado
-ECHO    para seus v°deos. Sugiro comeáar com o valor 256.
+ECHO    maior a possibilidade de bitrates menores. Experimente atÇ descobrir o valor adequado
+ECHO    para seus v°deos. Sugere-se comeáar com o valor 256 para avaliar os resultados.
 ECHO[ 
 set /p EXT=Digite a extens∆o dos arquivos a serem processados, sem utilizar ponto (ex: mp4):
 set /p BITRATE=Digite o bitrate desejado (ex: 200):
 ECHO[
-FOR %%i IN (*.%EXT%) DO ffmpeg -loglevel warning -stats -i "%%i" -ab 96k -b:v %BITRATE%k -preset veryfast "%%i_%BITRATE%k.mp4"
+FOR %%i IN (*.%EXT%) DO ffmpeg -loglevel warning -stats -i "%%i" -ab 80k -b:v %BITRATE%k -preset veryfast "%%i_%BITRATE%k.mp4"
 
