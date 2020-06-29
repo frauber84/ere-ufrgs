465da0532963548ec56800e36d3e1bd0c7b7be92
Fernando Rauber Gonçalves <36580373+frauber84@users.noreply.github.com>
2020-06-29 16:38:39 -0300

    Update comprime.bat

--- ffmpeg-acessibilidade/comprime.bat
+++ ffmpeg-acessibilidade/comprime.bat
@@ -8,22 +8,22 @@ GOTO FINAL
 
 :PROCESSAR
 ECHO ..........................................................
-ECHO     Comprime v¡deos com ffmpeg sem perda de resolu‡Æo       
+ECHO     Comprime vÂ¡deos com ffmpeg sem perda de resoluâ€¡Ã†o       
 ECHO         https://github.com/frauber84/ere-ufrgs/
 ECHO ..........................................................
 ECHO[
-ECHO Guia para bitrates (valores menores = mais compressÆo, menor tamanho do arquivo e qualidade)
+ECHO Guia para bitrates (valores menores = mais compressÃ†o, menor tamanho do arquivo e qualidade)
 ECHO[
-ECHO CompressÆo extrema = 128 (WhatsApp / 3G)
-ECHO CompressÆo alta = 256 (WhatsApp / 3G)
-ECHO CompressÆo m‚dia = 384 (qualidade m‚dia)
-ECHO CompressÆo baixa = 512 (qualidade alta)
+ECHO CompressÃ†o extrema = 128 (WhatsApp / 3G)
+ECHO CompressÃ†o alta = 256 (WhatsApp / 3G)
+ECHO CompressÃ†o mâ€šdia = 384 (qualidade mâ€šdia)
+ECHO CompressÃ†o baixa = 512 (qualidade alta)
 ECHO[
 ECHO OBS:
-ECHO 1) NÆo comprima v¡deos ao enviar para YouTube, envie na qualidade original.
-ECHO 2) O menor bitrate poss¡vel depende da resolu‡Æo original. Quanto menor a resolu‡Æo,
-ECHO    maior a possibilidade de bitrates menores. Experimente at‚ descobrir o valor adequado
-ECHO    para seus v¡deos. Sugere-se come‡ar com o valor 256 para avaliar os resultados.
+ECHO 1) NÃ†o comprima vÂ¡deos ao enviar para YouTube, envie na qualidade original.
+ECHO 2) O menor bitrate possÂ¡vel depende da resoluâ€¡Ã†o original. Quanto menor a resoluâ€¡Ã†o,
+ECHO    maior a possibilidade de bitrates menores. Experimente atâ€š descobrir o valor adequado
+ECHO    para seus vÂ¡deos. Sugere-se comeâ€¡ar com o valor 128 para avaliar os resultados.
 ECHO[ 
 set /p BITRATE=Digite o bitrate desejado e pressione ENTER:
 ECHO[ 
